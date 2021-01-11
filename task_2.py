089b59b84e044a900b08e9947d2ce841a1d9094d
Danityec <Danityec@gmail.com>
2021-01-12 00:38:17 +0200

    update task2+task3

--- task_2.py
+++ task_2.py
@@ -6,21 +6,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
+
 #def task2_func1():
 
 mobiles = pd.read_csv('mobile_price_1.csv', index_col="id")
-print(mobiles.head())
+#print(mobiles.head())
 df = mobiles.select_dtypes(np.number)
 corrMatrix = df.corr()
 # sns.heatmap(corrMatrix, annot=True, vmin=-1, vmax=1)
 # plt.show()
 
+
+
 #def task2_func1():
+# -------- categorical ordinali --------
+#mobiles_oh = pd.concat([mobiles, pd.get_dummies(mobiles['cores'])], axis=1)
+#mobiles_oh = pd.concat([mobiles, pd.get_dummies(mobiles['speed'])], axis=1)
+#mobiles_oh = pd.concat([mobiles, pd.get_dummies(mobiles['sim'])], axis=1)
+#mobiles_oh = pd.concat([mobiles, pd.get_dummies(mobiles['wifi'])], axis=1)
 
-cores_order = ['single', 'dual', 'triple', 'quad', 'penta', 'hexa', 'hepta', 'octa']
-mobiles.cores.value_counts().reindex(cores_order).plot.bar()
+#--------  categorical nominali ---------
+#mobiles_oh = pd.concat([mobiles, pd.get_dummies(mobiles['bluetooth'])], axis=1)
+mobiles_oh = pd.concat([mobiles, pd.get_dummies(mobiles['screen'])], axis=1)
+
+print(mobiles.head())
+corr_oh = mobiles_oh.corr()
+mask = np.triu(np.ones_like(corr_oh, dtype=bool))
+f, ax = plt.subplots(figsize=(15, 8))
+cmap = sns.diverging_palette(200, 10, as_cmap=True)
+sns.heatmap(corr_oh, mask=mask, cmap=cmap, center=0, square=True, linewidths=.5, vmax=1, vmin=-1)
 plt.show()
-
 
 #if __name__ == "__task_2__":
     #task2()
--- task_3.py
+++ task_3.py
@@ -1,1 +1,33 @@
 # DANIT
+
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+# def task3_func1():
+
+mobiles = pd.read_csv('mobile_price_1.csv', index_col="id")
+core_list = ['single', 'dual', 'triple', 'quad', 'penta', 'hexa', 'hepta', 'octa']
+mobiles['cores_ord'] = pd.Categorical(mobiles.cores, ordered=True, categories=core_list).codes+1
+speed_list = ['low', 'medium', 'high']
+mobiles['speed_ord'] = pd.Categorical(mobiles.speed, ordered=True, categories=speed_list).codes+1
+sim_list = ['Single', 'Dual']
+mobiles['sim_ord'] = pd.Categorical(mobiles.sim, ordered=True, categories=sim_list).codes
+wifi_list = ['none', 'n', 'g', 'b', 'a']
+mobiles['wifi_ord'] = pd.Categorical(mobiles.wifi, ordered=True, categories=wifi_list).codes+1
+bluetooth_list = ['No', 'Yes']
+mobiles['bluetooth_bin'] = pd.Categorical(mobiles.bluetooth, ordered=True, categories=bluetooth_list).codes
+screen_list = ['LCD', 'Touch']
+mobiles['screen_bin'] = pd.Categorical(mobiles.screen, ordered=True, categories=screen_list).codes
+
+df = mobiles.select_dtypes(np.number)
+corrMatrix = df.corr()
+sns.heatmap(corrMatrix, annot=True, vmin=-1, vmax=1)
+plt.show()
+print(mobiles.head(10))
+np.savetxt('mobile_prices_converted.csv', (mobiles), delimiter=',')
+
+
+
+
